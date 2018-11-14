e53da3fe943a18e5448736747ee5f9dc2a9b3969
mtumilowicz <michal.tumilowicz01@gmail.com>
2018-11-14 13:11:55 +0100

    DelegateOwnerThisTest.groovy

--- src/test/groovy/DelegateOwnerThisTest.groovy
+++ src/test/groovy/DelegateOwnerThisTest.groovy
@@ -22,7 +22,7 @@ class DelegateOwnerThisTest extends Specification {
         rehydratedClosure() == "this method"
     }
     
-    def "class This has field value"() {
+    def "class This has field value, and we use this.value"() {
         given:
         def closure = {
             methodFromDelegate(this.value)
@@ -34,25 +34,25 @@ class DelegateOwnerThisTest extends Specification {
         rehydratedClosure() == "fromThis"
     }
 
-    def "class This does not have field value but Owner has"() {
+    def "class This has field value, and we use just value"() {
         given:
         def closure = {
             methodFromDelegate(value)
         }
 
-        def rehydratedClosure = closure.rehydrate(new Delegate(), new Owner(), new EmptyThis())
+        def rehydratedClosure = closure.rehydrate(new Delegate(), new Owner(), new This())
 
         expect:
         rehydratedClosure() == "fromOwner"
     }
 
-    def "class This, Owner does not have field value"() {
+    def "Owner does not have field value"() {
         given:
         def closure = {
             methodFromDelegate(value)
         }
 
-        def rehydratedClosure = closure.rehydrate(new Delegate(), new EmptyOwner(), new EmptyThis())
+        def rehydratedClosure = closure.rehydrate(new Delegate(), new EmptyOwner(), new This())
 
         expect:
         rehydratedClosure() == "fromDelegate"
