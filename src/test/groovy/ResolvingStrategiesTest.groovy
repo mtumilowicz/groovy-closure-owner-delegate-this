67dbea38d0e344245d93b153377b05e60e598673
mtumilowicz <michal.tumilowicz01@gmail.com>
2018-11-14 14:42:54 +0100

    ResolvingStrategiesTest.groovy reformatting

--- src/test/groovy/ResolvingStrategiesTest.groovy
+++ src/test/groovy/ResolvingStrategiesTest.groovy
@@ -15,7 +15,7 @@ class ResolvingStrategiesTest extends Specification {
 
         and:
         def rehydratedClosure = closure.rehydrate(new Delegate(), new Owner(), new This())
-        
+
         when:
         rehydratedClosure.resolveStrategy = Closure.DELEGATE_FIRST
 
@@ -32,29 +32,29 @@ class ResolvingStrategiesTest extends Specification {
         and:
         def rehydratedClosure = closure.rehydrate(new Delegate(), new EmptyOwner(), new This())
         rehydratedClosure.resolveStrategy = Closure.OWNER_ONLY
-        
+
         when:
         rehydratedClosure()
 
         then:
         thrown(MissingPropertyException)
     }
-    
+
     def "to self"() {
         given:
         ExpandoMetaClass.enableGlobally()
-        
+
         and:
         def closure = {
             value
         }
-        
+
         and:
         closure.metaClass.value = "inClosure"
-        
+
         when:
         closure.resolveStrategy = Closure.TO_SELF
-        
+
         then:
         closure() == "inClosure"
     }
